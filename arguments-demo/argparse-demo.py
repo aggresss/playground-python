500987fac6cc14e068d9e8b8578cb645d6c02829
aggresss <aggresss@gmail.com>
2018-03-21 12:28:25 +0800

    ADD arguments-demo

---
+++ arguments-demo/argparse-demo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='argparse-demo.py', 
+        usage='%(prog)s [options] usage', 
+        description = 'demo - description',
+        epilog = 'demo - epilog'
+    )
+    
+    # option arguments
+    parser.add_argument('-v', '--version', action='version', version='version demo', help='Show program version info and exit.')
+    parser.add_argument('-f', '--foo', type=int, default=64)
+    parser.add_argument('-s', '--string', type=str, default = None)
+    parser.add_argument('-x', '--xxx', dest='yyy', type=int, default=128)
+    
+    # required arguments
+    parser.add_argument('require_str', type=str)
+    parser.add_argument('require_int', type=int)
+
+    args = parser.parse_args()
+    print args
+    
---
+++ arguments-demo/sysargv-demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+print "file = ", sys.argv[0]
+for i in range(1, len(sys.argv)):
+    print "parameter%s = %s"%(i, sys.argv[i])
