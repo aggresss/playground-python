fb485d980659b3b8eab74aed0106ac942e7f82fa
yujia <yujia@qiniu.com>
2020-12-11 14:10:41 +0800

    2020-12-11 14:10:41 CST W49D5

--- workbench/govendor-to-gomod/vendor-to-mod.py
+++ workbench/govendor-to-gomod/vendor-to-mod.py
@@ -4,6 +4,7 @@
 import sys
 import argparse
 import json
+from datetime import datetime
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -36,10 +37,47 @@ if __name__ == "__main__":
 
     argvs = parser.parse_args()
 
-    if argvs.vpath is None:
+    if argvs.vfile is None:
         print('please input vendor.json file path')
         sys.exit()
 
-    if argvs.mpath is None:
+    if argvs.mfile is None:
         print('please input go.mod file path')
         sys.exit()
+
+    with open(argvs.vfile, 'r') as vendor_file:
+        vendor_dict = json.load(vendor_file)
+        print(len(vendor_dict['package']))
+
+    if len(vendor_dict['package']) > 0:
+        with open(argvs.mfile, 'a') as mod_file:
+            mod_file.write("""
+require(
+""")
+            for index, element in enumerate(vendor_dict['package']):
+                # module path
+                mod_file.write('\t' + element['path'] + ' ')
+                # assemble version-date-revision
+                # 1. version
+                if 'version' in element and element['version'] != '':
+                    v_index = element['version'].find('v')
+                    mod_file.write(element['version'][v_index:])
+                else:
+                    mod_file.write('v0.0.0')
+                # 2. date
+                if 'revisionTime' in element and element['revisionTime'] != '':
+                    utc_time = datetime.strptime(
+                        element['revisionTime'],
+                        '%Y-%m-%dT%H:%M:%SZ')
+                    mod_file.write('-' + utc_time.strftime('%Y%m%d%H%M%S'))
+                else:
+                    mod_file.write('-197001010000')
+                # 3. revision
+                if 'revision' in element and element['revision'] != '':
+                    mod_file.write('-' + element['revision'][0:12])
+                else:
+                    mod_file.write('-000000000000')
+                # EOL
+                mod_file.write('\n')
+
+            mod_file.write(')\n')
